8683c2e2fc483e31ac4f0aff73626e347aef154a
m93enbah <mohammedenbah93@gmail.com>
2022-07-03 09:07:06 +0400

    vcbnvcbn

---
+++ MicroServiceToutarial/NewModule/project01 Develop Micorservice With MongoDb/src/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private IConfiguration Configuration { get; set; }
+        public CatalogContext(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+
+            var client = new MongoClient(Configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(Configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(Configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+
+
+            CatalogContext.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ MicroServiceToutarial/NewModule/project01 Develop Micorservice With MongoDb/src/Catalog.API/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+        [BsonElement("summary")]
+        public string Summary { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("imageFile")]
+        public string ImageFile { get; set; }
+
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+    }
+}
